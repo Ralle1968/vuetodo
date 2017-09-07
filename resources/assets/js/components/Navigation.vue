129843500c719347e28c2276e6cc7e369cd43f01
Ralle1968 <graef.ralf@gmail.com>
2017-09-08 01:07:43 +0200

    xyxyxx

--- resources/assets/js/components/Navigation.vue
+++ resources/assets/js/components/Navigation.vue
@@ -8,7 +8,7 @@
       <span class="icon-bar"></span>
     </button>
     <img src="img/todo.png" alt="Dispute Bills">
-    <a class="navbar-brand">Ralfs VueToDo-Lister</a>
+    <a class="navbar-brand">Ralfs VueToDo-SuperLister</a>
   </div>
 
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
